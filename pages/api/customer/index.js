9fcefe6510c71212761ab78b3a4faaf0fef02923
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-02-17 16:50:53 +0330

    connect to db

---
+++ pages/api/customer/index.js
@@ -0,0 +1,34 @@
+import Customer from "@/models/Customer";
+import connectDB from "@/utils/ConnectDB";
+
+export default async function handler(req, res) {
+  try {
+    await connectDB();
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ status: "failed", message: "Error in connecting to DB..." });
+    return;
+  }
+  if (req.method === "POST") {
+    const data = req.body.data;
+    if (!data.name || !data.lastName || !data.email) {
+      return res
+        .status(400)
+        .json({ status: "failed", message: "Invalid Data" });
+    }
+
+    try {
+      const customer = await Customer.create(data);
+      res
+        .status(201)
+        .json({ status: "seccess", message: "Data created.", data: customer });
+    } catch (error) {
+      console.log(error);
+      res
+        .status(500)
+        .json({ status: "failed", message: "Error in insert data" });
+    }
+  }
+}
